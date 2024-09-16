567fb663d92dacac33ed1c57687d44d84cf1d44c
YuriiDmytruk <yuriydmytrukr@gmail.com>
2024-09-16 15:30:53 +0300

    finish pieChart

--- charts/PieChartComponent.tsx
+++ charts/PieChartComponent.tsx
@@ -1,12 +1,98 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {PieChart} from 'react-native-gifted-charts';
+import {useSelector} from 'react-redux';
+import {RootState} from '../redux/store';
+import Loading from '../components/Loading';
+import NoData from '../components/NoData';
+import {CovidData} from '../redux/types';
+
+type pieDataItem = {
+  value: number;
+  text: string;
+};
 
 const PieChartComponent = () => {
+  const {data, isFetching} = useSelector((state: RootState) => state.covidData);
+  const [chartData, setCahrtData] = useState<pieDataItem[]>([]);
+
+  const [filter, setFilter] = useState<'new' | 'total'>('total');
+
+  useEffect(() => {
+    const prepareData = (): pieDataItem[] => {
+      return data.map((element: CovidData) => ({
+        text: element.region,
+        value:
+          element.region === ''
+            ? 0
+            : element.cases.reduce(
+                (accumulator, currentValue) =>
+                  accumulator + currentValue[filter],
+                0,
+              ),
+      }));
+    };
+
+    if (data[0]?.cases) {
+      setCahrtData(prepareData());
+    }
+  }, [filter, data]);
+
   return (
-    <View>
-      <Text>PieChartComponent</Text>
+    <View style={styles.container}>
+      {isFetching ? (
+        <Loading />
+      ) : data[0]?.cases ? (
+        <View style={styles.chart_container}>
+          <PieChart data={chartData} labelsPosition="onBorder" showText={true} />
+          <View style={styles.button_container}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => setFilter('total')}>
+              <Text style={styles.button_text}>Total</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => setFilter('new')}>
+              <Text style={styles.button_text}>New</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        <NoData />
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 10,
+    paddingTop: 30,
+  },
+  chart_container:{
+    width: '100%',
+    alignItems: 'center',
+  },
+  button_container: {
+    paddingTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    gap: 20,
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderColor: '#2525f5',
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  button_text: {
+    fontSize: 15,
+    color: '#2525f5',
+  },
+});
 
 export default PieChartComponent;
